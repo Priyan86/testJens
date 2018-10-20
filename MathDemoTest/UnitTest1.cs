b3ef6d9bd38a3cdcfb8683f67ade07683832048e
Priyan86 <bk.priyadharrsan@gmail.com>
2018-10-20 20:03:20 +0530

    First Commit

---
+++ MathDemoTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathematicalDemo;
+
+namespace MathDemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program pgm = new Program();
+
+            Assert.AreEqual(7, pgm.SummationValue(6, 1));
+        }
+    }
+}
---
+++ MathematicalDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalDemo
+{
+    public class Program
+    {
+         void Main(string[] args)
+        {
+           int value = SummationValue(4, 3);
+           
+            Console.WriteLine("Summed up value is : " + value);
+        }
+
+        public int SummationValue(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
